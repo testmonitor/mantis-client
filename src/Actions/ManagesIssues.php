1b07f86d0ed3178948d1f1cd74f19309cf775fbd
cam.lafit <cam.lafit@azerttyu.net>
2021-04-29 18:19:36 +0200

    Get all issues
    * solve #8

--- src/Actions/ManagesIssues.php
+++ src/Actions/ManagesIssues.php
@@ -19,7 +19,13 @@ trait ManagesIssues
      */
     public function issues($projectId)
     {
-        $result = $this->get('issues', ['query' => ['project_id' => $projectId]]);
+        $result = [];
+        $page_nb = 1;
+        do {
+            $result_page = $this->get('issues', ['query' => ['project_id' => $projectId,'page'=>$page_nb]]);
+            $result = array_merge_recursive($result,$result_page);
+            $page_nb++;
+        } while(!empty($result_page['issues']));
 
         Validator::isArray($result);
 
